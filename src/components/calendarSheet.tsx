f527047a93c30a1f1a99abcb84eb5a23642e8a8a
Prasoon Mahawar <prasoonmahawar21@gmail.com>
2021-10-11 01:35:31 +0530

    finalizing task

--- src/components/calendarSheet.tsx
+++ src/components/calendarSheet.tsx
@@ -4,25 +4,25 @@ import Grid from "@mui/material/Grid";
 import { IconButton } from '@mui/material';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import {displayEvents } from '../indexedDB/indexedDb'
 
 const useStyles = makeStyles(() => ({
     root: {
         background: 'white',
         height: '700px',
         width: '100%',
-        marginLeft: '2px',
-        
+        paddingTop: '142px'
 
     },
     grid: {
         borderBottom: "1px solid #cfcfcf",
         borderLeft: '1px solid #cfcfcf',
-        flexGrow: 1,
+        flex: '1 1 0',
         minHeight: '60px',
         height: '60px'
     },
     header: {
-        flexGrow: 1,
+        flex: '1 1 0',
         height: '84px',
         display: 'grid',
         alignItems: 'self-end'
@@ -58,7 +58,10 @@ const useStyles = makeStyles(() => ({
         height: '25px',
         width: '100%',
         display:'flex',
-        padding: '1em'
+        padding: '1em 0',
+        position: 'fixed',
+        background: 'white',
+        zIndex: 2000
     },
     monthText: {
         color: '#3c4043',
@@ -109,11 +112,46 @@ const useStyles = makeStyles(() => ({
             content: "''",
             position: 'absolute',
             height: '12px',
-            marginLeft: '-122.5px',
+            marginLeft: '-52%',
             marginTop: '-7px',
             width: '12px',
             zIndex: '505'
         }
+    },
+    eventCard:{
+        borderRadius: "4px",
+        marginTop: "1px",
+        outline: 'none',
+        zIndex: 5,
+        backgroundColor: 'rgb(33, 255, 6)',
+        borderColor: 'rgb(22, 204, 0)',
+        position: 'relative',
+        overflow:'hidden'
+    },
+    eventTitle:{
+        maxHeight: '15px',
+        display: 'flex',
+        wordBreak: 'break-word',
+        fontSize: '12px',
+        paddingLeft: '5px',
+        fontWeight: 500,
+        textAlign: 'left',
+        overflow: 'hidden'
+    },
+    eventTime:{
+        maxHeight: '15px',
+        display: 'flex',
+        wordBreak: 'break-word',
+        fontSize: '12px',
+        paddingTop: '3px',
+        paddingLeft: '5px'
+    },
+    dateHeader:{
+        width: '100%',
+        position: 'fixed',
+        top: '57px',
+        background: 'white',
+        zIndex: 2000
     }
    
 }))
@@ -132,16 +170,11 @@ function CalendarSheet(){
         return date.getMinutes() - 1 
     });
     let dayToday = todayDate.getDay()
-    const [timeZone,setTimeZone] = useState(() => {
-        let zone = todayDate.toString()!.match(/GMT\+[0-9]+/)![0]
-        return zone 
-    })
-    
-    // if (typeof timeZone === 'string') {
-    //     timeZone = timeZone?.match(/GMT\+[0-9]+/)[0]
-    // }
-    // console.log()
-    // timeZone = timeZone && timeZone.match(/GMT\+[0-9]+/)[0]
+    // const [timeZone,setTimeZone] = useState(() => {
+    //     let zone = todayDate.toString()!.match(/GMT\+[0-9]+/)![0]
+    //     return zone 
+    // })
+    let timeZone = todayDate.toString()!.match(/GMT\+[0-9]+/)![0]
     const [thisWeek,setThisWeek] = useState(() => {
         let start = 0;
         let end=6;
@@ -165,26 +198,58 @@ function CalendarSheet(){
         return startWeek
     })
     
+    const [events,setEvents] = useState<any>([])
+    const [db,setDb] = useState<any>(null)
+    
     function getWeek(date:Date,direction:String){
         let fromDate = new Date(date);
-        console.log(fromDate)
         let week = [0,1,2,3,4,5,6].map(i=>{
             return new Date(date)
         });
         if(direction === "next"){
             for(let i=1;i<=7;i++){
                 week[i-1].setDate(fromDate.getDate() + i)
-                console.log(week[i-1])
             }
-            console.log(week)
             setThisWeek(week)
+            let start= new Date(week[0])
+            let end = new Date(week[6])
+            start.setHours(0,0,0,0)
+            end.setHours(23,59,59)
+
+            displayEvents(db,start.getTime(),end.getTime()).then((request)=>{
+            request.onsuccess = function(event:any) {
+                console.log(event.target.result)
+                // return event.target.result
+                setEvents(event.target.result)
+            };
+            request.onerror = function(err:any) {
+                return []
+            };
+               
+            })
+
         }
         if(direction === "prev"){
             for(let i=1;i<=7;i++){
                 week[7-i].setDate(fromDate.getDate() - i)
             }
-            // console.log(week)
             setThisWeek(week)
+           
+            let start= new Date(week[0])
+            let end = new Date(week[6])
+            start.setHours(0,0,0,0)
+            end.setHours(23,59,59)
+
+            displayEvents(db,start.getTime(),end.getTime()).then((request) => {
+                request.onsuccess = function(event:any) {
+                    console.log(event.target.result)
+                    // return event.target.result
+                    setEvents(event.target.result)
+                };
+                request.onerror = function(err:any) {
+                    return []
+                };
+            })
         }
         
     }
@@ -198,13 +263,61 @@ function CalendarSheet(){
         setCurrentHour(hour)
         setCurrentMin(minute-1)
     }
+    function calculateCardHeight(duration:number):number{
+        let multiplier = Math.floor(duration/15);
+
+        return (multiplier*15)
+
+    }
+    function calculateCardTop(dateTime:number):string{
+        let date = new Date(dateTime)
+        let minutes = date.getMinutes()
+        let multiplier = Math.floor(minutes/15);
+        return `${(multiplier*15)}px`
+    }
+    function calculateCardWidth(dateTime:number):string{
+        return '100%'
+    }
+    function calculateTimeString(dateTime:number,duration:number):string{
+        let date = new Date(dateTime);
+        let startHour = date.getHours() === 0?12:(date.getHours()%12);
+        let startMinutes = date.getMinutes()
+        let quotient = Math.floor((startMinutes+duration)/60);
+        let remainder = (startMinutes+duration) % 60;
+
+        return `${startMinutes >0 ? (startHour+':'+startMinutes):startHour} ${(date.getHours()<12 && (date.getHours()+quotient)>=12)?'am':''} - ${remainder>0?(((startHour+quotient)%12 === 0 ?12:(startHour+quotient)%12)+':'+remainder):(startHour+quotient)%12 === 0 ?12:(startHour+quotient)%12} ${(date.getHours()+quotient)<12?'am':'pm'}`
+    }
     useEffect(() => {
+        let DBOpenRequest = window.indexedDB.open("calEvents",2);
+        DBOpenRequest.onerror = function(event) {
+            console.log("Error opening the db")
+        };
+            
+        DBOpenRequest.onsuccess = async function(event) {
+            setDb(DBOpenRequest.result);
+            let start = new Date(thisWeek[0])
+            let end = new Date(thisWeek[6])
+            start.setHours(0,0,0,0)
+            end.setHours(23,59,59)
+            displayEvents(DBOpenRequest.result,start.getTime(),end.getTime()).then((request)=>{
+                request.onsuccess = function(event:any) {
+                    console.log(event.target.result)
+                    // return event.target.result
+                    setEvents(event.target.result)
+                };
+                request.onerror = function(err:any) {
+                    return []
+                };
+            })
+            
+        }
         const interval = setInterval(calculateTimeSliderPosition,10000)
         return ()=> clearInterval(interval)
-    },[])
+    },[thisWeek])
     return (
         <React.Fragment>
             <div className={classes.weekNavigator}>
+            <div className={classes.time}></div>
                     <IconButton sx={{ color:"#5f6368"}} onClick={() => { getWeek(thisWeek[0],"prev"); }}>
                         <NavigateBeforeIcon />
                     </IconButton>
@@ -217,7 +330,7 @@ function CalendarSheet(){
             </div>
             
             <Grid container className={classes.root}>
-            <Grid container  style={{width: '100%'}}>
+            <Grid container  className={classes.dateHeader}>
                 <Grid container style={{  marginTop: 2}} item  spacing={0}>
                 <div className={classes.timeZone}>{timeZone}</div>
                     {thisWeek.map((value,index) => (
@@ -234,8 +347,16 @@ function CalendarSheet(){
                 <Grid key={hourValue} container item  spacing={0}>
                 {hourValue === 0 ?<div className={classes.time}></div> :<div className={classes.time}>{hourValue > 11?`${hourValue === 12?hourValue:hourValue%12} PM`:`${hourValue} AM`}</div>}
                 {thisWeek.map((value,index) => (
-                    <Grid key={index}  item  spacing={0} className={classes.grid}>
+                    <Grid key={index}  item className={classes.grid} >
                         {value.getDate() === todayDate.getDate() && hourValue === currentHour && <div className={classes.timeSlider} style={{ top: (currentMin)+'px'}}></div>}
+                        {events.length>0 && events.map((e:any)=>{
+                            let ev:any = new Date(e.dateTime)
+                            let evMins = ev.getMinutes()
+                            return (value.getDate() === ev.getDate() && ev.getHours() === hourValue && <div className={classes.eventCard} style={{ height: calculateCardHeight(e.duration)+'px', top: calculateCardTop(e.dateTime), width: calculateCardWidth(e.dateTime)}}>
+                                <div className={classes.eventTitle} style={{paddingTop: calculateCardHeight(e.duration) <= 30?'0px':'3px'}}>{calculateCardHeight(e.duration) <= 30?e.title+', '+(ev.getHours()===0?12:ev.getHours()%12)+(evMins>0?evMins<10?':0'+evMins:':'+evMins:'')+(ev.getHours()<12?'am':'pm'):e.title}</div>
+                                { calculateCardHeight(e.duration) > 30 &&<div className={classes.eventTime}>{calculateTimeString(e.dateTime,e.duration)}</div>}
+                            </div>)
+                        })}
                     </Grid>
                 ))}
                 </Grid>
--- src/indexedDB/indexedDb.ts
+++ src/indexedDB/indexedDb.ts
@@ -1,20 +1,92 @@
-
-
-let db;
-let DBOpenRequest = window.indexedDB.open("calEvents",1);
-
-DBOpenRequest.onerror = function(event) {
-    console.log("Error opening the db")
+let db:any;
+let objectStore:any
+const IDB = async function(){
+  return new Promise(function(res) {
+    let DBOpenRequest = window.indexedDB.open("calEvents",2);
+  
+    DBOpenRequest.onerror = function(event) {
+        console.log("Error opening the db")
+      };
+      
+    DBOpenRequest.onsuccess = async function(event) {
+      
+        db = DBOpenRequest.result;
+        await clearData()
+        res(await dumpDataFromService())
+    }
+    DBOpenRequest.onupgradeneeded = function(event) {
+      db=DBOpenRequest.result
+      if(!db.objectStoreNames.contains('events')){
+        objectStore = db.createObjectStore('events', {
+          keyPath : 'eventId'
+        })
+        objectStore.createIndex('dateTimeIdx','dateTime',{unique:false})
+      }
+      
+    }
+  })
+  
+}
+const clearData = async function () {
+  
+  let txn = db.transaction("events", "readwrite");
+  txn.oncomplete = function(event:any) {
+    console.log('db cleared')
   };
-  
-DBOpenRequest.onsuccess = function(event) {
-  
-    db = DBOpenRequest.result;
-    displayEvents();
-};
 
-const displayEvents = function(){
+  txn.onerror = function(err:any) {
+    console.log(err)
+  };
 
+  let store = txn.objectStore("events");
+
+  let request = store.clear();
+  request.onsuccess = async function(event:any) {
+  };
+  request.onerror = function(err:any) {
+  };
+}
+const dumpDataFromService = async function(){
+  fetch('http://localhost:8000/events').then(async (res) => {
+    let body = await res.json()
+    console.log(body)
+    let txn = db.transaction('events','readwrite');
+    txn.oncomplete = () => {
+      console.log("txn complete");
+    };
+    txn.onerror = (err:any) => {
+      console.warn("error in txn")
+    }
+    let store = txn.objectStore('events');
+    body.forEach(async (element: Object) => {
+      let request = await store.add(element)
+      request.onsuccess = () => {
+        console.log("successfully added an object")
+      }
+      request.onerror = (err :any) => {
+        console.log(err)
+      }
+    });
+  }).catch(err => {
+    console.log(err)
+  })
 }
 
-export default {}
+
+async function displayEvents(db:any,start:Number,end:Number):Promise<any>{
+  let txn = db.transaction("events", "readonly");
+  txn.oncomplete = function(event:any) {
+    console.log('fetched records complete')
+  };
+
+  txn.onerror = function(err:any) {
+    console.log(err)
+  };
+
+  let store = txn.objectStore("events");
+  let idx = store.index("dateTimeIdx")
+  let request = idx.getAll(IDBKeyRange.bound(start,end,false,false));
+  return request;
+}
+
+export {IDB,dumpDataFromService,displayEvents}
